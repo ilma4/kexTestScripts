596ae917036aef3d20485412b9bdda0d53ccef4b
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-01-11 20:50:00 +0100

    Refactor run_test function and add command line arguments handling

--- run-test.py
+++ run-test.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 import os
+import sys
 import shutil
 import subprocess
 
 outputdir = "./temp"
 mockitoPath = "runtime-deps/lib/mockito-core-4.11.0.jar"
+dryRun = False
 
-def run_test(target):
+def run_test(target: str):
     target = target.replace("/", ".")
     print(f"Running test: {target}")
-    output = target.split('.')[-1]
+    className = target.split('.')[-1]
     print(f"target: {target}")
-    print(f"output: {output}")
+    print(f"className: {className}")
     deps = [ "commons-collections/target/commons-collections4-4.5-SNAPSHOT.jar",
     "commons-collections/target/dependency/error_prone_annotations-2.18.0.jar",
     "commons-collections/target/dependency/junit-jupiter-params-5.9.3.jar",
@@ -42,25 +44,27 @@ def run_test(target):
         deps.append(mockitoPath)
 
     classpath = ":".join(deps)      
+    output = os.path.join(outputdir, className)
+    print(f"Output directory: {output}")
+
     command = [
         "./kex.py",
         "--classpath", classpath,
         "--target", target,
         "--mode", "concolic",
-        "--output", f"{outputdir}/{output}"
+        "--output",  output   
     ]
 
-    shutil.rmtree(f"{outputdir}/{output}", ignore_errors=True)
+    if dryRun:
+        return
+
+    shutil.rmtree(output, ignore_errors=True)
     subprocess.run(command, stdout=subprocess.DEVNULL)
 
 
 def test_all():
     filename = "./test-targets.txt"
     
-
-    # Cleanup existing output directory
-    if os.path.exists(outputdir):
-        os.system(f"rm -r {outputdir}")
 
     # Read test targets from file
     filename = "./test-targets.txt"
@@ -71,21 +75,21 @@ def test_all():
             run_test(target)
 
 
-def multiple_mode():
+def multiple_mode(targets: list[str]):
     print("Testing both kex and clean-kex")
     os.chdir("./kex")
-    single_mode()
+    single_mode(targets)
     print("Switching to clean-kex")
     os.chdir("../clean-kex")
-    single_mode()
+    single_mode(targets)
     print("All done!")
 
 
-def single_mode():
+def single_mode(targets: list[str]):
     print("Current working directory: " + os.getcwd())
 
-    if len(os.sys.argv) > 1:
-        for target in os.sys.argv[1:]:
+    if len(targets) != 0:
+        for target in targets:
             run_test(target)
         return
 
@@ -94,12 +98,28 @@ def single_mode():
 
 
 def main():
+    global outputdir 
+    global dryRun
+
+    targets = list[str]()
+    args = iter(sys.argv[1:])
+    for arg in args:
+        if arg == "--output":
+            outputdir = next(args)
+        elif arg == "--dry-run":
+            dryRun = True
+            print("Dry run mode. No tests will be run and no files will be created or deleted")
+        else:
+            targets.append(arg)
+
+    print(f"Targets from command line: {targets}")
+
     if os.listdir().__contains__("kex.py"):
         print(f"Testing only {os.path.basename(os.getcwd())}")
-        single_mode()
+        single_mode(targets=targets)
         return
     elif os.listdir().__contains__("kex") and os.listdir().__contains__("clean-kex"):
-        multiple_mode()
+        multiple_mode(targets=targets)
         return
     else:
         print("Script must be run from kex directory (contains kex.py) or superdirectory containing both kex and clean-kex (clean-kex must be named that)")
